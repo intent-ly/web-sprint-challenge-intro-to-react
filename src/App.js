1fe013f16dc0c7d1ed5b963939a940fb01fab2b2
intent-ly <erick.skillsusa@gmail.com>
2020-10-09 10:52:43 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Character from './components/Character'
 import './App.css';
+import Axios from 'axios'
 
 const App = () => {
+  const [baseURL] = useState('https://rickandmortyapi.com/api/character/')
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    Axios.get(baseURL)
+    .then((res) => {
+      console.log(res)
+      setCharacters(res.data.results)
+    }) .catch((err) => {
+      console.log('There is an error', err)
+    })
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +27,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters</h1>
+      {
+        characters.map((character, index) => (
+          <Character key= {`app-character-map-${index}`} character={character} />
+        ))
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) =>{
+    console.log(props)
+    return(
+        <div class ='card'>
+            <img src={props.character.image} />
+            <h2>{props.character.name}</h2>
+            <p>Status : {props.character.status}</p>
+        </div>
+    );
+}
+
+export default Character;
